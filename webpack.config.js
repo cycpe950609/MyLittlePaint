82e367eaa9779980a1c8094acad7870a1b5ee6ef
Ting Fang, Tsai <cycpe950609@gmail.com>
2026-03-15 17:06:27 +0800

    REFACTOR: Remove `__webpack_public_path__` and use webpack-dev-server instead of LiveServer

--- webpack.config.js
+++ webpack.config.js
@@ -3,48 +3,37 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import CopyPlugin from "copy-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import CompressionPlugin from 'compression-webpack-plugin';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-import RemarkHTML from "remark-html";
-import { CleanWebpackPlugin } from "clean-webpack-plugin";
-import CompressionPlugin from 'compression-webpack-plugin';
-import webpack from "webpack";
 
 const module = {
     entry: {
         index: "./ts/main.ts"
     },
     devServer: {
-        static: {
-            directory: path.join(__dirname, 'public'),
-        },
         compress: true,
         port: 8080,
     },
     output: {
         filename: "js/[name].bundle.js",
         chunkFilename: "js/[name].chunk.js",
-        publicPath: "public/",
-        path: path.resolve(__dirname, "public/")
     },
     plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            filename: 'index.html',
+        }),
         new CopyPlugin({
             patterns: [
                 { from: "favicon.ico", to: "favicon.ico" },
-                { from: "index.html", to: "index.html" },
                 { from: "css", to: "css" },
                 { from: "img", to: "img" }
             ]
         }),
         new CompressionPlugin(),
-        new webpack.DefinePlugin({
-            'process.env.ASSET_PATH': JSON.stringify(`./`),
-        }),
-        // new CleanWebpackPlugin({
-        //     dangerouslyAllowCleanPatternsOutsideProject: true,
-        //     dry: false
-        // })
     ],
     module: {
         rules: [
@@ -76,27 +65,11 @@ const module = {
                     }
                 }
             },
-            {
-                test: /\.html$/,
-                exclude: /node_modules/,
-                use: { loader: 'html-loader' }
-            },
-            {
-                test: /\.md$/,
-                use: [
-                    {
-                        loader: "html-loader"
-                    },
-                    {
-                        loader: "remark-loader",
-                        options: {
-                            remarkOptions: {
-                                plugins: [RemarkHTML]
-                            }
-                        }
-                    }
-                ]
-            },
+            // {
+            //     test: /\.html$/,
+            //     exclude: /node_modules/,
+            //     use: { loader: 'html-loader' }
+            // },
             {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"]
