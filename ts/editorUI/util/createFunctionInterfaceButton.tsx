e533d8575432b2c77ac934f651b0169ae5e7c364
Ting Fang, Tsai <cycpe950609@gmail.com>
2026-03-16 10:56:41 +0800

    REFACTOR: Use anchor to style and layout the tips of ToolbarItem

--- ts/editorUI/util/createFunctionInterfaceButton.tsx
+++ ts/editorUI/util/createFunctionInterfaceButton.tsx
@@ -4,29 +4,55 @@ import { Div, Span } from "./Element";
 const createFunctionInterfaceButton = (
     func: FunctionInterface
 ) => {
-    // const [tip, setTip] = useState("");
 
-    return <Div className="toolbar-item"
-        // onmouseover={
-        //     () =>{ 
-        //         if(func.Tip === undefined) return;
-        //         setTip( typeof func.Tip === "string" ? func.Tip : func.Tip() );           
-        //     }
-        // }
-        onclick={
-            () => window.editorUI.Mode.changeFunction(func)
-        }
-        $style={{
-            backgroundImage: "url(img/" + (func.ImgName !== undefined ? func.ImgName : "color_selector") + ".svg)"
-        }}
-    >
-        {
-            func.Tip !== undefined &&
-            <Span className="tooltip-text">
-                {typeof func.Tip === "string" ? func.Tip : func.Tip()}
-            </Span>
-        }
-    </Div>
+    return <>
+
+        <Div className="toolbar-item">
+            <Div className="toolbar-button"
+                // onmouseover={
+                //     () =>{ 
+                //         if(func.Tip === undefined) return;
+                //         setTip( typeof func.Tip === "string" ? func.Tip : func.Tip() );           
+                //     }
+                // }
+                onclick={
+                    () => window.editorUI.Mode.changeFunction(func)
+                }
+                $style={{
+                    backgroundImage: "url(img/" + (func.ImgName !== undefined ? func.ImgName : "color_selector") + ".svg)"
+                }}
+            />
+
+            {
+                func.Tip !== undefined &&
+                <Span className="tooltip-text">
+                    {typeof func.Tip === "string" ? func.Tip : func.Tip()}
+                </Span>
+            }
+        </Div >
+    </>
+
+    // return <Div className="toolbar-item"
+    //     // onmouseover={
+    //     //     () =>{ 
+    //     //         if(func.Tip === undefined) return;
+    //     //         setTip( typeof func.Tip === "string" ? func.Tip : func.Tip() );           
+    //     //     }
+    //     // }
+    //     onclick={
+    //         () => window.editorUI.Mode.changeFunction(func)
+    //     }
+    //     $style={{
+    //         backgroundImage: "url(img/" + (func.ImgName !== undefined ? func.ImgName : "color_selector") + ".svg)"
+    //     }}
+    // >
+    //     {
+    //         func.Tip !== undefined &&
+    //         <Span className="tooltip-text">
+    //             {typeof func.Tip === "string" ? func.Tip : func.Tip()}
+    //         </Span>
+    //     }
+    // </Div>
 }
 
 export default createFunctionInterfaceButton;
