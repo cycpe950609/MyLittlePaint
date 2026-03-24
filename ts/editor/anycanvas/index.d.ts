22d56fcedea64dc36c9baaa111b20727d1ea2edf
Ting Fang, Tsai <cycpe950609@gmail.com>
2026-03-24 17:06:45 +0800

    FIXED: `[vite] Internal server error: Failed to resolve import "./anycanvas"` NOTE: We need real `index.ts` not type-only `index.d.ts` (this won't exist in runtime)

--- ts/editor/anycanvas/index.d.ts
+++ ts/editor/anycanvas/index.d.ts
@@ -5,32 +5,6 @@
  *      Export declare     
  */
 
-export declare const AnyCanvas: {
-    // CVS
-    WebCanvas: typeof import("./canvas").WebCanvas,
-    InfiniteCanvas: typeof import("./infinite").InfiniteCanvas,
-    // Layer
-    Layer: typeof import("./cvs/layer").Layer,
-    // Shapes
-    Shape: {
-        Line: typeof import("./cvs/shape").Line;
-        Rect: typeof import("./cvs/shape").Rect;
-        Circle: typeof import("./cvs/shape").Circle;
-        Ellipse: typeof import("./cvs/shape").Ellipse;
-        Path: typeof import("./cvs/shape").Path;
-        Text: typeof import("./cvs/shape").Text;
-
-        Config: {
-            Line: import("./cvs/shape").LineConfig;
-            Rect: import("./cvs/shape").RectConfig;
-            Circle: import("./cvs/shape").CircleConfig;
-            Ellipse: import("./cvs/shape").EllipseConfig;
-            Path: import("./cvs/shape").PathConfig;
-            Text: import("./cvs/shape").TextConfig;
-        }
-    }
-}
-
 // type namespace
 export namespace AnyCanvas {
     // CVS
