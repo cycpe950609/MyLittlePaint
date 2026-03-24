24b033fa8c6222310f692299881685bc7df13620
Ting Fang, Tsai <cycpe950609@gmail.com>
2026-03-24 16:12:22 +0800

    WIP: Manage view in `CanvasBase`

--- ts/editor/anycanvas/cvs/canvas.ts
+++ ts/editor/anycanvas/cvs/canvas.ts
@@ -15,6 +15,9 @@ import { ViewManager } from "./view";
 export class CanvasBase {
 
     protected View: ViewManager;
+    protected view_height: number = 0;
+    protected view_width: number = 0;
+
     protected container: HTMLDivElement;
     protected backgroundCVS: BackgroundCanvas;
 
@@ -43,23 +46,56 @@ export class CanvasBase {
         return rtv;
     };
 
+    // Viewport
     public viewAt(center: Point, rotDeg: number, scale: number) {
-
+        this.View.viewAt(center, rotDeg, scale);
+    }
+    public viewCenterAt(center: Point) {
+        this.View.viewCenterAt(center);
+    }
+    public viewRotDegAt(rotDeg: number) {
+        this.View.viewRotDegAt(rotDeg);
+    }
+    public viewScaleAt(scale: number) {
+        this.View.viewScaleAt(scale);
+    }
+    // Move the View along the axis of view
+    public viewUp(deltaY: number) {
+        this.View.viewUp(deltaY);
+    }
+    public viewDown(deltaY: number) {
+        this.View.viewDown(deltaY);
+    }
+    public viewRight(deltaX: number) {
+        this.View.viewRight(deltaX);
+    }
+    public viewLeft(deltaX: number) {
+        this.View.viewLeft(deltaX);
+    }
+    // Scale
+    public viewZoomIn(scale: number) {
+        this.View.viewZoomIn(scale);
+    }
+    public viewZoomOut(scale: number) {
+        this.View.viewZoomOut(scale);
+    }
+    // Rotation
+    public viewRotate(degree: number, rot_center?: Point) {
+        this.View.viewRotate(degree, rot_center);
     }
 
-    // Properties
     public get viewHeight(): number {
-        throw new Error()
+        return this.view_height
     }
     public set viewHeight(height: number) {
-        throw new Error()
+        this.view_height = height;
     }
 
     public get viewWidth(): number {
-        throw new Error()
+        return this.view_width;
     }
     public set viewWidth(width: number) {
-        throw new Error()
+        this.view_width = width;
     }
 
 };
