cdc0fc3f7d4b4c05cedf7b85db6add81ee5b91aa
Ting Fang, Tsai <cycpe950609@gmail.com>
2026-04-16 16:52:19 +0800

    ADDED: Add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     base: './',
+    plugins: [
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'My Little Paint',
+                short_name: 'Painter',
+                theme_color: '#000000',
+                icons: [{
+                    src: "icons/icon-512.svg",
+                    sizes: '512x512',
+                    type: 'image/svg+xml',
+                    purpose: 'any',
+                }]
+            },
+            devOptions: {
+                enabled: true
+            }
+        })
+    ],
     server: {
         port: 8080,
     },
